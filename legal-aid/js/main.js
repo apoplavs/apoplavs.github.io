a3ff6e8032c6d4471e322f4ba47b3e5070c2346a
apoplavs <p.andriy.v@gmail.com>
2017-10-30 14:19:45 +0200

    separate js to other file

---
+++ legal-aid/js/main.js
@@ -0,0 +1,26 @@
+var tabs = $('.tabs > li');
+
+		tabs.on("click", function(){
+			$('#floatingCirclesG').show();
+			$('#content').hide();
+			tabs.removeClass('active');
+			$(this).addClass('active');
+			$.get($(this).find('a').attr('f-link'), function(data) {
+				$('#content').html(data);
+			});
+		});
+		$(document).ajaxComplete(function() {
+			setTimeout(function () {
+				$('#floatingCirclesG').hide();
+				$('#content').show();
+			}, 100);
+		});
+		$(document).ready(function() {
+			$('#content').hide();
+			$.ajax({
+				url: $('.tabs > li.active').find('a').attr('f-link'),
+				success: function(response) {
+				$('#content').html(response);
+				}
+			})
+		});
