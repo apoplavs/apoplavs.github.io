de5573950ee28cda3dc788d9f0fb208f95876f6f
apoplavs <p.andriy.v@gmail.com>
2018-01-12 17:46:00 +0200

    begin open-court landing page

---
+++ open-court/js/landing-page.js
@@ -0,0 +1,6 @@
+//https://developers.google.com/chart/interactive/docs/gallery/piechart#fullhtml
+//https://habrahabr.ru/post/304296/
+//http://htmlbook.ru/blog/grafiki-i-diagrammy
+//  wix
+// https://editor.wix.com/html/editor/web/renderer/edit/e54ac290-1436-460f-ad20-8e37687048ae?editorSessionId=87BA4C4B-9880-401B-B6FE-9498AE5A0EEA&metaSiteId=e0f5f35b-a339-4a73-86e8-71d3d6446ff6
+// https://pandriyv.wixsite.com/opencourt
